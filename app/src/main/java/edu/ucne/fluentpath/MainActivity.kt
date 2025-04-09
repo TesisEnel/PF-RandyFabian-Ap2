d25bfc56f8b966790b9e03da0229f50f82b4499e
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2025-04-09 09:59:10 -0400

    Modificnado MainActivity

--- app/src/main/java/edu/ucne/fluentpath/MainActivity.kt
+++ app/src/main/java/edu/ucne/fluentpath/MainActivity.kt
@@ -1,26 +1,36 @@
+
 package edu.ucne.fluentpath
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.fluentpath.ui.theme.FluentPathTheme
+import edu.ucne.fluentpath.ui.theme.MainApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            android.util.Log.e("APP_CRASH", "Crash detectado", throwable)
+            recreate()
+        }
+
         setContent {
             FluentPathTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFF38CC74)
+                ) {
+                    MainApp()
                 }
             }
         }
